52c839d255b4fdbf12bd1d01bd115ae4f0b1533c
valefresia <131785137+valefresia@users.noreply.github.com>
2026-08-27 10:34:31 -0300

    Serverless function conectada a Gemini funcionando en local con vercel dev

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import { renderChat } from "./views/chat.js";
 import { renderAbout } from "./views/about.js";
 import { renderNotFound } from "./views/notFound.js";
 
-// Mapa de rutas: cada path apunta a la funcion que dibuja esa vista
 const routes = {
   "/": renderHome,
   "/home": renderHome,
@@ -11,7 +10,6 @@ const routes = {
   "/about": renderAbout,
 };
 
-// Lee la URL actual y ejecuta la funcion de render correspondiente
 export function router() {
   const path = window.location.pathname;
   const render = routes[path] || renderNotFound;
